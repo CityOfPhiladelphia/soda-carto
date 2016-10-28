ac44469a1f95f49a9076d28694f28d73bbcbe58d
timwis <tim@timwis.com>
2016-10-28 11:06:24 -0400

    add server, readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# soda-carto
+Query [Carto](http://carto.com) datasets with a Socrata-style
+[SODA2](https://dev.socrata.com/docs/queries/) API
+
+## Install
+1. Clone this repository
+2. Install dependencies via `npm install`
+3. Copy `.env.test` to `.env` and fill in the `CARTO_ENDPOINT` (ex. `https://phl-admin.carto.com/api/v2/`)
+4. Run a server via `npm start`
+
+## Usage
+```
+http://localhost:8080/resource/incidents?$select=*&$where=location_b = '100 BLOCK W GIRARD AVE'
+```
+
---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const url = require('url')
+const serverRouter = require('server-router')
+const request = require('request')
+require('dotenv').load({ silent: true })
+
+const convertRequest = require('./lib').convertRequest
+
+const endpoint = process.env.CARTO_ENDPOINT
+const port = process.env.PORT || 8080
+
+const router = serverRouter([
+  ['/resource/:resource', resourceHandler]
+])
+
+http.createServer((req, res) => router(req, res).pipe(res)).listen(port)
+
+function resourceHandler (req, res, params) {
+  const query = url.parse(req.url, true).query
+  const sql = convertRequest(params.resource, query)
+  const cartoUrl = `${endpoint}sql/?format=GeoJSON&q=${sql}`
+  return request(cartoUrl)
+}
+
